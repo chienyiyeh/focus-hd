6100c62f20dfe286bd51b58bbe0f308309904127
chienyiyeh <ironyeh45@gmail.com>
2026-03-29 15:59:04 +0800

    Delete Logout.php

--- Logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_name('KANBAN_SESSION');
-session_start();
-$_SESSION = [];
-session_destroy();
-setcookie('remember_token', '', time() - 3600, '/');
-header('Location: login.php');
-exit;
